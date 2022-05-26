b588ffafd069dfa4ffd5eb5f089dc80979569431
mateonav98 <mateonav98@gmail.com>
2022-05-26 16:15:21 -0700

    update manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,9 +27,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Text Editor',
+        short_name: 'jate',
+        description: 'Just another text editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
