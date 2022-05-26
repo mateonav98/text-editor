9e837c7e57df4ef3d5c863db2532db4384b643da
mateonav98 <mateonav98@gmail.com>
2022-05-25 20:46:41 -0700

    add html webpack plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,8 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,9 +16,13 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
       
     ],
-
+    // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
         {
